518115eeb83d219e07305cb1fbcd4f2811188021
James Fryman <james@frymanet.com>
2011-08-23 17:20:02 -0500

    trying to do stupid debugging

--- tweeter.rb
+++ tweeter.rb
@@ -128,14 +128,8 @@ class TweeterPlugin < Plugin
   end
   
   def get_json(url)
-    uri = URI.parse(url)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-    JSON.parse(response.body)
+    data = @bot.httputil.get(url)
+    JSON.parse(data)
   end
   
   def save_value(prefix,identifier, val)
