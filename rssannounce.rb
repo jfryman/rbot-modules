c7973a7788535381271056670db0f5893754e0bb
James Fryman <james@frymanet.com>
2011-08-24 15:18:33 -0500

    reducing the update time to 5 seconds for debug purposes

--- rssannounce.rb
+++ rssannounce.rb
@@ -25,7 +25,7 @@ class RssannouncePlugin < Plugin
       end
     end
     
-    time_frame = 600
+    @time_frame = 5
     @registry.keys.each { |key|
       if key =~ /username\|/
         # Get the url from our feed key
@@ -34,7 +34,7 @@ class RssannouncePlugin < Plugin
         # This does a few things:  
         #   - Creates a new timer. 
         #   - Stores the timer action_id
-        save_value("action", url, add_timer(user, time_frame))
+        save_value("action", url, add_timer(user, @time_frame))
       
         # Stagger our saved feed updates, so we're not hammering RSS feeds.
         time_frame += 60
@@ -94,10 +94,10 @@ class RssannouncePlugin < Plugin
     end
   end
   
-  def add_timer(feed, time_period=5.0)
+  def add_timer(feed, time_period=@time_frame)
     channel = get_value('channel', feed)
     if not time_period
-      time_period = 5.0
+      time_period = @time_frame
     end
     
     save_value('nextupdate', feed, (Time.now + time_period).strftime("%Y%m%d%H%M%S"))
